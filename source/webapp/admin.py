527208b3ebe0a33c236fdefb5c11569918f44298
Abylay Kuvatov <abukuvatov@icloud.com>
2023-02-25 11:58:06 +0600

    Создал модель гостевой книги, зарегистрировал модель в адмиин-панели, а также создал суперпользователя

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from webapp.models import GuestBook
+
+
 # Register your models here.
+class GuestBookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'email', 'text', 'status', 'created_at', 'edited_at', 'is_deleted', 'deleted_at']
+    list_filter = ['author', 'email', 'status', 'created_at']
+    search_fields = ['author', 'email', 'status']
+    fields = ['author', 'email', 'text', 'status', 'created_at']
+    list_editable = ['author', 'email', 'text', 'status', 'is_deleted']
+    readonly_fields = ['created_at', 'edited_at', 'deleted_at']
+
+
+admin.site.register(GuestBook, GuestBookAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.db.models import TextChoices
+from django.utils import timezone
+
+
+class StatusChoice(TextChoices):
+    ACTIVE = 'active', 'Активно'
+    BLOCKED = 'blocked', 'Заблокировано'
+
 
 # Create your models here.
+class GuestBook(models.Model):
+    author = models.CharField(max_length=20, null=False, blank=False, verbose_name="Имя автора")
+    email = models.EmailField(max_length=254, unique=True, null=False, blank=False, verbose_name="Email")
+    text = models.TextField(max_length=2000, null=False, blank=False, verbose_name="Текст")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата и время создания")
+    edited_at = models.DateTimeField(auto_now=True, verbose_name="Дата и время изменения")
+    status = models.CharField(max_length=20, null=False, blank=False, choices=StatusChoice.choices,
+                              default=StatusChoice.ACTIVE, verbose_name="Статус")
+    is_deleted = models.BooleanField(null=False, default=False, verbose_name="Удален")
+    deleted_at = models.DateTimeField(null=True, default=None, verbose_name="Дата и время удаления")
+
+    def __str__(self):
+        return f'{self.author} | {self.email} | {self.status}'
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_deleted = True
+        self.deleted_date = timezone.now()
+        self.save()
